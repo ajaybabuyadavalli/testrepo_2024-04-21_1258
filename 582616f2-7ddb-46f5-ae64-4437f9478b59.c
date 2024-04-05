5bf104d9156697ce1ddaa955e3ca564f9378b015
ajaybabuy2k <ayadava1@asu.edu>
2024-04-05 18:01:32 +0000

    Commit by ajaybabuy2k 8f559d1d-98be-441f-b605-6c434e28f806

---
+++ 582616f2-7ddb-46f5-ae64-4437f9478b59.c
@@ -0,0 +1,7 @@
+# B'eget' b'u
+# B'erat' b'ad' b
+B'erat' b'at'
+# B'eros' b'eu' 
+B'cras' b
+B'eros' 
+B'odio' b'v
---
+++ e720f28a-3841-4b73-9679-4f7e9f00264f.c
@@ -0,0 +1,7 @@
+B'arcu' b'at'. 
+B'duis'
+B'elit' b'eu' 
+B'nibh' b'at' 
+B'cras' b'mi' b
+# B'urna' b'at' b
+B'elit' b'i
